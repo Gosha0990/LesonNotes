5bb376db73bb99079fb5ad1be40bda7f6ce5b072
Георгий Владимирович Барышников <anny1280008@gmail.com>
2022-02-08 22:24:47 +0300

    Mapping

---
+++ Neotes.Application/Common/Mappings/IMapWidth.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neotes.Application.Common.Mappings
+{
+    public class IMapWidth <T>
+    {
+        void Mapping(Profile profile) =>
+                profile.CreateMap(typeof(T), GetType());
+    }
+}
